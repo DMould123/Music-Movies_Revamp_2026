df8aa53fc737ed5b389d0c528b1e6645f6f6292f
David Mould <david.mould123@yahoo.com>
2026-01-30 16:36:28 +0100

    Remove React Query DevTools from production
    - Removed ReactQueryDevtools import and component
    - DevTools was showing flower icon with query status (fresh, fetching, paused)
    - This debugging tool was overlapping footer text on smaller screens
    - Users don't need this in normal app usage

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './components/Home'
@@ -47,7 +46,6 @@ export default function App() {
                 />
               </Routes>
               <Footer />
-              <ReactQueryDevtools initialIsOpen={false} />
             </div>
           </Router>
         </QueryClientProvider>
