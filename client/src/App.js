0918322d62e94e0fbf3b432259dc61402458d793
David Mould <david.mould123@yahoo.com>
2026-02-03 00:41:52 +0100

    Implemented page transition animations for improved UX flow

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import PageTransition from './components/PageTransition'
 import Home from './components/Home'
 import Albums from './components/Albums'
 import Contact from './components/Contact'
@@ -35,17 +36,19 @@ export default function App() {
                   toastOptions={{ duration: 2000, className: 'toast-y2k' }}
                 />
                 <Routes>
-                  <Route element={<Home />} path="/" />
-                  <Route element={<Albums />} path="/albums" />
-                  <Route element={<Contact />} path="/contact" />
-                  <Route element={<Login />} path="/login" />
-                  <Route element={<Register />} path="/register" />
-                  <Route element={<MovieDetails />} path="/movies/:id" />
+                  <Route element={<PageTransition><Home /></PageTransition>} path="/" />
+                  <Route element={<PageTransition><Albums /></PageTransition>} path="/albums" />
+                  <Route element={<PageTransition><Contact /></PageTransition>} path="/contact" />
+                  <Route element={<PageTransition><Login /></PageTransition>} path="/login" />
+                  <Route element={<PageTransition><Register /></PageTransition>} path="/register" />
+                  <Route element={<PageTransition><MovieDetails /></PageTransition>} path="/movies/:id" />
                   <Route
                     element={
-                      <ProtectedRoute>
-                        <Dashboard />
-                      </ProtectedRoute>
+                      <PageTransition>
+                        <ProtectedRoute>
+                          <Dashboard />
+                        </ProtectedRoute>
+                      </PageTransition>
                     }
                     path="/dashboard"
                   />
---
+++ client/src/components/PageTransition.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/pageTransition.css'
+
+const PageTransition = ({ children }) => {
+  return <div className="page-transition">{children}</div>
+}
+
+export default PageTransition
